bdbb5d58fc55ab4736eee5879b563c8bfa868a7a
ngando03 <dongan03102003@gmail.com>
2025-01-03 01:08:28 +0700

    CREATE_PROJECT

---
+++ DoThiKimNgan-1148-2425-BaiThi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoThiKimNgan_1148_2425_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
