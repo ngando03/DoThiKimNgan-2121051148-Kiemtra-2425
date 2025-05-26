c22bcd445462c00fc9e9bef1d59ecb6e4017efc2
ngando03 <dongan03102003@gmail.com>
2025-05-26 09:14:36 +0700

    lan2

---
+++ DoThiKimNgan-1148-2425-BaiThi/Controllers/K2Controller.cs
@@ -0,0 +1,27 @@
+﻿
+using DoThiKimNgan_1148_2425_BaiThi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DoThiKimNgan_1148_2425_BaiThi.Controllers;
+
+public class K2Controller : Controller
+{
+    private readonly ILogger<K2Controller> _logger;
+
+    public K2Controller(ILogger<K2Controller> logger)
+    {
+        _logger = (ILogger<K2Controller>?)logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+}
---
+++ DoThiKimNgan-1148-2425-BaiThi/Models/K2.cs
@@ -0,0 +1,11 @@
+﻿namespace DoThiKimNgan_1148_2425_BaiThi.Models
+{
+    public class K2
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        
+        
+    }
+}
